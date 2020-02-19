b70729c06ea48c4bc05325b840a44a4163955050
MrGravey <gmark@pdx.edu>
2020-02-19 00:28:53 -0800

    Added DNA Bases set to DNA Class. RandomMutate method added to DNA Class.

--- 8queens.py
+++ 8queens.py
@@ -1,8 +1,13 @@
+import random
+
+QUEEN_BASES = [1, 2, 3, 4, 5, 6, 7, 8]
+
 class DNA(): 
 
-  def __init__(self, DNA=[], MaxLen=8):
+  def __init__(self, DNA=[], Bases=set, MaxLen=8):
     self.MaxLen = MaxLen
     self.DNA = []
+    self.Bases = Bases
     if(len(DNA) == self.MaxLen):
       self.DNA = DNA 
     
@@ -26,12 +31,19 @@ class DNA():
       rightSplit.append(x)
     return rightSplit
 
+  def randomMutate(self) -> bool:
+    if(len(self.Bases) == 0): return False
+    randIndex = random.randint(0, self.MaxLen)
+    randBase = random.randint(0, len(self.Bases))
+    self.DNA[randIndex] = randBase
+    return True
+
 def generateChild(parent1, parent2, crossPos):
-  child = DNA(parent1.leftSplit(crossPos) + parent2.rightSplit(crossPos))
+  child = DNA(parent1.leftSplit(crossPos) + parent2.rightSplit(crossPos), QUEEN_BASES)
   print(child.get())
 
-parent1 = DNA([1, 2, 3, 4, 5, 6, 7 ,8])
-parent2 = DNA([4, 5, 7, 8, 4, 3, 2, 5])
+parent1 = DNA([1, 2, 3, 4, 5, 6, 7 ,8], QUEEN_BASES)
+parent2 = DNA([4, 5, 7, 8, 4, 3, 2, 5], QUEEN_BASES)
 
 print(parent1.get())
 print(parent2.get())
