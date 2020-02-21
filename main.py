57be99c3869a264807c3d0d509daa2d29070326c
MrGravey <gmark@pdx.edu>
2020-02-21 03:52:00 -0800

    Added command line arguments to main.py to select a custom population size and mutation chance. README updated to reflect these changes.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import random
 from eightQueens import *
 from DNA import DNA
@@ -6,12 +7,30 @@ from Generation import Generation
 POPULATION_SIZE = 100
 MUT_CHANCE = 0.10
 
+# Command line argument checking and assignment
+if(len(sys.argv) > 1):
+  if(len(sys.argv) == 3):
+    if(float(sys.argv[2]) < 0.0 or float(sys.argv[2]) > 1.0):
+      print("main: invalid entry for MUTATION_CHANCE. Valid entries: 0 <= x <= 1")
+      sys.exit(1)
+    else:
+      POPULATION_SIZE = int(sys.argv[1])
+      MUT_CHANCE = float(sys.argv[2])
+  else:
+    print("main: invalid number of arguments.")
+    print("main: python3 main.py int float.")
+    print("main: python3 main.py POPULATION_SIZE MUTATION_CHANCE")
+    sys.exit(1)
+
+# Generate random DNAs for a initial generation
 oldGen = Generation([], POPULATION_SIZE)
 for x in range(0, POPULATION_SIZE):
   oldGen.add(generateRandomDNA())
 
+# Solve for 8queens
 goalDNA = solve(oldGen, POPULATION_SIZE, MUT_CHANCE)
 
+# Print results
 if(goalDNA != None):
   print("Initial Setup*")
   print(oldGen.get()[0].get())
