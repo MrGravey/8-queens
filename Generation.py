48f9692f087b6f167bc6f137373392559cf5945f
MrGravey <gmark@pdx.edu>
2020-03-29 17:20:31 -0700

    Cleaned up code with python black and added pre-commit for it

--- Generation.py
+++ Generation.py
@@ -1,71 +1,76 @@
 from DNA import DNA
 
-class Generation():
-  
-  def __init__(self, Generation=[], MaxSize=10):
-    self.CurrSize = 0
-    self.MaxSize = MaxSize
-    self.Generation = []
-    self.SelectionChance = []
 
-    if(len(Generation) <= MaxSize):
-      self.CurrSize = len(Generation)
-      self.Generation = Generation
-      self._generateSelectionChance()
-  
-  def _generateSelectionChance(self):
-    if(self.CurrSize < self.MaxSize): return False
-    self.SelectionChance =  []
-    generationFitness = self.getFitness()
-    for x in self.Generation:
-      chance = x.getFitness()/generationFitness
-      self.SelectionChance.append(chance)
-    return True
+class Generation:
+    def __init__(self, Generation=[], MaxSize=10):
+        self.CurrSize = 0
+        self.MaxSize = MaxSize
+        self.Generation = []
+        self.SelectionChance = []
 
-  def generateSelectionChance(self):
-    self._generateSelectionChance()
+        if len(Generation) <= MaxSize:
+            self.CurrSize = len(Generation)
+            self.Generation = Generation
+            self._generateSelectionChance()
 
-  def add(self, DNA: DNA) -> bool:
-    if(self.CurrSize >= self.MaxSize): return False
-    
-    self.Generation.append(DNA)
-    self.CurrSize += 1
-    self._generateSelectionChance()
-    return True
+    def _generateSelectionChance(self):
+        if self.CurrSize < self.MaxSize:
+            return False
+        self.SelectionChance = []
+        generationFitness = self.getFitness()
+        for x in self.Generation:
+            chance = x.getFitness() / generationFitness
+            self.SelectionChance.append(chance)
+        return True
 
-  def get(self):
-    return self.Generation
+    def generateSelectionChance(self):
+        self._generateSelectionChance()
 
-  def size(self):
-    return self.CurrSize
+    def add(self, DNA: DNA) -> bool:
+        if self.CurrSize >= self.MaxSize:
+            return False
 
-  def isEmpty(self):
-    return True if self.CurrSize <=0 else False
+        self.Generation.append(DNA)
+        self.CurrSize += 1
+        self._generateSelectionChance()
+        return True
 
-  def isFull(self):
-    if(self.CurrSize >= self.MaxSize): return True
-    return False
-       
-  def getSelectionChance(self):
-    return self.SelectionChance
+    def get(self):
+        return self.Generation
 
-  def getSelectionIndex(self, chance: int) -> int:
-    index = 0
-    currSum = 0
-    for x in self.SelectionChance:
-      currSum += self.SelectionChance[index]
-      if(currSum < chance):
-        index += 1
-      else: break
-    return index
+    def size(self):
+        return self.CurrSize
 
-  def getFitness(self):
-    fitness = 0
-    for x in self.Generation:
-      fitness += x.getFitness()
+    def isEmpty(self):
+        return True if self.CurrSize <= 0 else False
 
-    return fitness
+    def isFull(self):
+        if self.CurrSize >= self.MaxSize:
+            return True
+        return False
 
-  def getAverageFitness(self) -> float:
-    if(self.CurrSize <= 0): return -1
-    return self.getFitness()/self.CurrSize
+    def getSelectionChance(self):
+        return self.SelectionChance
+
+    def getSelectionIndex(self, chance: int) -> int:
+        index = 0
+        currSum = 0
+        for x in self.SelectionChance:
+            currSum += self.SelectionChance[index]
+            if currSum < chance:
+                index += 1
+            else:
+                break
+        return index
+
+    def getFitness(self):
+        fitness = 0
+        for x in self.Generation:
+            fitness += x.getFitness()
+
+        return fitness
+
+    def getAverageFitness(self) -> float:
+        if self.CurrSize <= 0:
+            return -1
+        return self.getFitness() / self.CurrSize
--- eightQueens.py
+++ eightQueens.py
@@ -8,101 +8,107 @@ MAX_FITNESS = 28
 
 # Checks for collisions between points(row, col)
 def isCollision(x1: int, y1: int, x2: int, y2: int):
-  # Vertical check (should never occurr)
-  if(x1 == x2): return True
-  # Horizontal check
-  if(y1 == y2): return True
+    # Vertical check (should never occurr)
+    if x1 == x2:
+        return True
+    # Horizontal check
+    if y1 == y2:
+        return True
 
-  comp1 = x1 - x2 if (x1 > x2) else x2 - x1
-  comp2 = y1 - y2 if (y1 > y2) else y2 - y1
+    comp1 = x1 - x2 if (x1 > x2) else x2 - x1
+    comp2 = y1 - y2 if (y1 > y2) else y2 - y1
 
-  # Diagonal check
-  if(comp1 == comp2): return True
+    # Diagonal check
+    if comp1 == comp2:
+        return True
 
-  # No collision
-  return False
+    # No collision
+    return False
+
 
 # Generates the fitness score of a DNA
 def fitnessFunc(individual: DNA) -> int:
-  DNA = individual.get()
-  maxIndex = len(DNA)
-  currIndex = 0
-  fitness = 0
+    DNA = individual.get()
+    maxIndex = len(DNA)
+    currIndex = 0
+    fitness = 0
 
-  for val1 in DNA[:maxIndex]:
-    x1 = currIndex
-    y1 = val1
-    subCurrIndex = currIndex+1
+    for val1 in DNA[:maxIndex]:
+        x1 = currIndex
+        y1 = val1
+        subCurrIndex = currIndex + 1
 
-    for val2 in DNA[currIndex+1:maxIndex]:
-      x2 = subCurrIndex
-      y2 = val2
-      if(isCollision(x1, y1, x2, y2)):
-        fitness += 1
-      subCurrIndex += 1  
+        for val2 in DNA[currIndex + 1 : maxIndex]:
+            x2 = subCurrIndex
+            y2 = val2
+            if isCollision(x1, y1, x2, y2):
+                fitness += 1
+            subCurrIndex += 1
 
-    currIndex += 1
+        currIndex += 1
 
-  return MAX_FITNESS - fitness
+    return MAX_FITNESS - fitness
+
 
 # Generates a new child DNA from two parents swapping DNA from both at a DNA cross position.
 def generateChild(parent1: DNA, parent2: DNA, crossPos: int, mutChance: float) -> DNA:
-  child = DNA(parent1.leftSplit(crossPos) + parent2.rightSplit(crossPos), QUEEN_BASES)
-  if(random.random() <= mutChance):
-    child.randomMutate()
-  child.setFitness(fitnessFunc(child))
-  return child
+    child = DNA(parent1.leftSplit(crossPos) + parent2.rightSplit(crossPos), QUEEN_BASES)
+    if random.random() <= mutChance:
+        child.randomMutate()
+    child.setFitness(fitnessFunc(child))
+    return child
+
 
 # Generates a random DNA
 def generateRandomDNA() -> DNA:
-  newDNA = []
-  for base in range(0, 8):
-    newDNA.append(QUEEN_BASES[random.randint(0, 7)])
+    newDNA = []
+    for base in range(0, 8):
+        newDNA.append(QUEEN_BASES[random.randint(0, 7)])
 
-  newDNA = DNA(newDNA, QUEEN_BASES)
-  newDNA.setFitness(fitnessFunc(newDNA))
-  return newDNA
+    newDNA = DNA(newDNA, QUEEN_BASES)
+    newDNA.setFitness(fitnessFunc(newDNA))
+    return newDNA
+
 
 # Prints the queen's board setup
 def printBoard(individual: DNA):
-  if(individual is not None):
-    for row in range(0, len(individual)):
-      print("|", end='')
-      for col in range(0, len(individual)):
-        if(individual[col] == row):
-          print("Q|", end='')
-        else:
-          print("-|", end='')
-      print()
+    if individual is not None:
+        for row in range(0, len(individual)):
+            print("|", end="")
+            for col in range(0, len(individual)):
+                if individual[col] == row:
+                    print("Q|", end="")
+                else:
+                    print("-|", end="")
+            print()
 
 
 # Genetic Algorithm
 def solve(oldGen: Generation, populationSize: int, mutChance: float) -> DNA:
-  goalDNA = None
-  genCountMax = 100000//oldGen.size()
-  genCount = 0
-  isGoal = False
-  while(not isGoal and genCount < genCountMax):
-    print(str(oldGen.getAverageFitness()))
-    newGen = Generation([], populationSize)
-    while(not newGen.isFull()):
-      parent1 = oldGen.get()[oldGen.getSelectionIndex(random.random())]
-      parent2 = oldGen.get()[oldGen.getSelectionIndex(random.random())]
-      crossPot = random.randint(0, len(QUEEN_BASES)-1)
-      child = generateChild(parent1, parent2, crossPot, mutChance)
-      if(child.getFitness() >= MAX_FITNESS): 
-        goalDNA = child.get()
-        isGoal = True
-      newGen.add(child) 
-      child = generateChild(parent2, parent1, crossPot, mutChance)
-      if(child.getFitness() >= MAX_FITNESS): 
-        goalDNA = child.get()
-        isGoal = True
-      newGen.add(child)
+    goalDNA = None
+    genCountMax = 100000 // oldGen.size()
+    genCount = 0
+    isGoal = False
+    while not isGoal and genCount < genCountMax:
+        print(str(oldGen.getAverageFitness()))
+        newGen = Generation([], populationSize)
+        while not newGen.isFull():
+            parent1 = oldGen.get()[oldGen.getSelectionIndex(random.random())]
+            parent2 = oldGen.get()[oldGen.getSelectionIndex(random.random())]
+            crossPot = random.randint(0, len(QUEEN_BASES) - 1)
+            child = generateChild(parent1, parent2, crossPot, mutChance)
+            if child.getFitness() >= MAX_FITNESS:
+                goalDNA = child.get()
+                isGoal = True
+            newGen.add(child)
+            child = generateChild(parent2, parent1, crossPot, mutChance)
+            if child.getFitness() >= MAX_FITNESS:
+                goalDNA = child.get()
+                isGoal = True
+            newGen.add(child)
 
-    oldGen = newGen
-    genCount += 1
+        oldGen = newGen
+        genCount += 1
 
-  print("Generations: " + str(genCount))
-  return goalDNA
-
+    print("Generations: " + str(genCount))
+    return goalDNA
